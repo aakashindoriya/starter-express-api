c2ae8a5c2a5bce0f40cb7b1dc74f0d5da79610d2
aakash <37771235+aakashindoriya@users.noreply.github.com>
2023-01-04 14:23:37 +0530

    json

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
